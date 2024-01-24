f5e23fa331db7eb7f52fb906d7094123e70080eb
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-01-24 16:06:08 +0200

    update Navigate blade php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,4 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 sticky     right-0 left-0 top-0 z-10">
-
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -11,7 +10,6 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800"/>
                     </a>
                 </div>
-
 
                 {{--Home--}}
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
